7f8a1b6daf044bed7d2941ab42a5bbb37316ca5e
Alhussein Zaghloul <152105415+AlhusseinZaghloul@users.noreply.github.com>
2024-10-16 16:19:49 +0300

    Make IBAN dynamic using faker library

--- tests/POM/addNewAccountTest.spec.ts
+++ tests/POM/addNewAccountTest.spec.ts
@@ -1,9 +1,10 @@
 import  {test, expect} from '@playwright/test' 
 import { LoginPage } from './login.page.ts' 
 import { AddNewBankAccount } from './addNewBankAccountPage.ts'
+import { faker } from '@faker-js/faker';
 
 test.describe('Add new', () => {
-  
+    const randomIban = faker.finance.iban();
     test('Add new Bank Account', async ({page}) => {
         const login= new  LoginPage(page) 
         await login.NavigateToLoginPage('')
@@ -11,7 +12,7 @@ test.describe('Add new', () => {
         await expect(page).toHaveURL('/dashboard')
         const addNewAccount= new AddNewBankAccount(page)
         await addNewAccount.navigateToAddNewBankAccountPage("http://ahq-systest-01:8090/finance/cash-and-bank/setup/bank-accounts/new-bank-account")
-        await addNewAccount.fillNewBankAccountdata("CIB","Smouha","Current Account","5467476sa5645srfew")
+        await addNewAccount.fillNewBankAccountdata("CIB","Smouha","Current Account",randomIban)
         await addNewAccount.clickOnSave()
         await expect(page).toHaveURL('finance/cash-and-bank/setup/bank-accounts')
 
